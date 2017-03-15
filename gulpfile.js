2bb25781f459a57f2008a9bfeedd57b59546ab82
akushka <artur.kushka@scout24.com>
2017-03-15 16:13:10 +0100

    Implemented production build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,33 @@ const browserify = require('browserify')
 const source = require('vinyl-source-stream')
 const babelify = require('babelify')
 const webserver = require('gulp-webserver')
+const clean = require('gulp-clean')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const runSequence = require('run-sequence')
+const uglify = require('gulp-uglify')
+const buffer = require('vinyl-buffer')
+const envify = require('gulp-envify')
+
+gulp.task('clean', () => {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+})
+
+gulp.task('copy:statics', () => {
+    return gulp.src(['./static/**/*'])
+               .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('build:css', () => {
+  return gulp.src('./static/assets/css/*.css')
+             .pipe(uglifycss({
+               "maxLineLen": 80,
+               "uglyComments": true
+              }))
+              .pipe(concat('style.css'))
+              .pipe(gulp.dest('./dist'))
+})
 
 gulp.task('build:es6', () => {
     return browserify({
@@ -22,20 +49,32 @@ gulp.task('build:es6', () => {
             this.emit('end')
         })
         .pipe(source('app.js'))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('./dist'))
 })
 
-gulp.task('build:statics', () => {
-    return gulp.src(['./static/**/*'])
-               .pipe(gulp.dest('./dist'))
+gulp.task('uglify:js', () => {
+  return gulp.src('./dist/app.js')
+             .pipe(buffer())
+             .pipe(envify('production'))
+             .pipe(uglify())
+             .pipe(gulp.dest('./dist'))
 })
 
-gulp.task('watch', ['build:es6', 'build:statics'], () => {
+gulp.task('dist:development', callback =>
+  runSequence('clean', 'copy:statics', 'build:css', 'build:es6', callback)
+)
+
+gulp.task('dist:production', callback => {
+  process.env.NODE_ENV = 'production'
+  return runSequence('clean', 'copy:statics', 'build:css', 'build:es6', 'uglify:js', callback)
+})
+
+gulp.task('watch', ['dist:development'], () => {
     gulp.watch('./src/**/*.es6', ['build:es6'])
-    gulp.watch('./static/**/*', ['build:statics'])
+    gulp.watch('./static/**/*', ['dist:development'])
 })
 
-gulp.task('serve', ['watch'], () => {
+gulp.task('serve', () => {
   gulp.src('dist')
     .pipe(webserver({
       livereload: true,
@@ -44,4 +83,4 @@ gulp.task('serve', ['watch'], () => {
     }))
 })
 
-gulp.task('default', ['serve'])
+gulp.task('default', ['watch', 'serve'])
